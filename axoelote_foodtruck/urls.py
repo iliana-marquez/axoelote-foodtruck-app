bcf457619f5f4ce0ff7695aaab0865e20136c0ba
iliana-marquez <y5d7l1us@students.codeinstitute.net>
2025-09-13 16:59:25 +0200

    fix: add app name to views for better tracking

--- axoelote_foodtruck/urls.py
+++ axoelote_foodtruck/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from home import views as index_views
+from home import views as home_views
 
 
 urlpatterns = [
-    path('', index_views.index, name="index"),
+    path('', home_views.index, name="index"),
     path('admin/', admin.site.urls),
 ]
