9124da4f4b0c3dd6cbe9c1efb8abe2b58da3f3fa
iliana-marquez <y5d7l1us@students.codeinstitute.net>
2025-09-13 16:12:58 +0200

    deployment prep: add heroku and localholst to ALLOWED_HOSTS, set new SECRET_KEY for security and hide it, set DEBUG = 'DYNO' not in os.environ for true/false dev/prod environment

--- axoelote_foodtruck/settings.py
+++ axoelote_foodtruck/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c^9jnhpy1%0n0tit^4!_8m8q$^_gcpsn+45s-nps%l3i6+f%!%'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DYNO' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
