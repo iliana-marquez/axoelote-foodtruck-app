750b6f4db43a8e5f8c67f7248c3611438f724370
iliana-marquez <y5d7l1us@students.codeinstitute.net>
2025-09-14 14:23:05 +0200

    enhance(BookingRequest): description and message made optional for ux mitigation, only to be filled for open events or special requirements.

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import BookingRequest
 
 # Register your models here.
+@admin.register(BookingRequest)
+class BookingRequestAdmin(SummernoteModelAdmin):
+    summenote_fields = ('description', 'message')
+    list_display = ['customer', 'event_title', 'start_datetime', 'status']
+    list_filter = ['status', 'event_type']
