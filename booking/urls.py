7ba15edd5cdaf9b046978df984149f521087f3b2
iliana-marquez <y5d7l1us@students.codeinstitute.net>
2025-12-14 19:34:49 +0100

    fix: Booking edit functionality will be merged into the booking detail view, which will be transformed to a class to have both get en post methods for cleaner and scalable code

--- booking/urls.py
+++ booking/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('request/', views.booking_request, name='booking_request'),
     path('bookings/', views.BookingList.as_view(), name='bookings'),
     path('<int:pk>/', views.booking_detail, name='booking_detail'),
-    path('<int:pk>/edit/', views.booking_edit, name='booking_edit'),
     path('slots/<str:date_str>/', views.get_slots_for_date, name='get_slots'),
 ]
